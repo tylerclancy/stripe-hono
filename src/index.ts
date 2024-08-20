6fcb216542e15865147fbed9ef3933eb510b7264
tyler <tylerclancy030@gmail.com>
2024-08-20 12:55:24 -0500

    Checkout session

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
+import { HTTPException } from 'hono/http-exception';
+import 'dotenv/config';
+
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2024-06-20',
+});
 
 const app = new Hono();
 
@@ -9,6 +16,36 @@ app.get('/', (c) => {
 
 app.post('/', (c) => {
   return c.text('Hi Hono!');
+});
+
+app.post('/checkout', async (c) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price: 'price_1PpvebD4FvYVlZiqB9x3gJsx',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'http://localhost:3000/success',
+      cancel_url: 'http://localhost:3000/cancel',
+    });
+
+    return c.json(session);
+  } catch (error: any) {
+    console.error(error);
+    throw new HTTPException(500, { message: error?.message });
+  }
+});
+
+app.get('/success', (c) => {
+  return c.text('Thanks for your purchase!');
+});
+
+app.get('/cancel', (c) => {
+  return c.text('Your purchase has been canceled!');
 });
 
 const port = 3000;
